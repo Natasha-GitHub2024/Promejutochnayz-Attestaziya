8d61e3a428a384f2f8a569a92fd9c0f8f303aa43
Natasha-GitHub2024 <156940213+Natasha-GitHub2024@users.noreply.github.com>
2024-06-24 20:08:48 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System; 
+ 
+class Program 
+{ 
+    static void Main() 
+    { 
+        Console.Write("Введите значение M: "); 
+        int M = int.Parse(Console.ReadLine()); 
+ 
+        Console.Write("Введите значение N: "); 
+        int N = int.Parse(Console.ReadLine()); 
+ 
+        PrintNaturalNumbers(M, N); 
+    } 
+ 
+    static void PrintNaturalNumbers(int current, int N) 
+    { 
+        if (current <= N) 
+        { 
+            Console.Write(current + " "); 
+            PrintNaturalNumbers(current + 1, N); 
+        } 
+    } 
+}
+
