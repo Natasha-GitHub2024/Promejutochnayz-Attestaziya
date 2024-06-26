5fc08588ef37492e97403e33120c16a44f4c0b8e
Natasha-GitHub2024 <156940213+Natasha-GitHub2024@users.noreply.github.com>
2024-06-26 15:11:54 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System; 
- 
-class Program 
+ class Program 
 { 
     static void Main() 
     { 
@@ -23,3 +22,50 @@ class Program
     } 
 }
 
+using System; 
+ class Program 
+{ 
+    static void Main() 
+    { 
+        Console.Write("Введите неотрицательное число m: "); 
+        int m = int.Parse(Console.ReadLine()); 
+ 
+        Console.Write("Введите неотрицательное число n: "); 
+        int n = int.Parse(Console.ReadLine()); 
+ 
+        int result = AckermannFunction(m, n); 
+        Console.WriteLine($"Значение функции Аккермана для ({m}, {n}) равно: {result}"); 
+    } 
+ 
+    static int AckermannFunction(int m, int n) 
+    { 
+        if (m == 0) 
+            return n + 1; 
+        else if (n == 0) 
+            return AckermannFunction(m - 1, 1); 
+        else 
+            return AckermannFunction(m - 1, AckermannFunction(m, n - 1)); 
+    } 
+}
+
+using System; 
+ 
+class Program 
+{ 
+    static void Main() 
+    { 
+        int[] myArray = { 10, 20, 30, 40, 50 }; 
+ 
+        Console.WriteLine("Элементы массива, начиная с конца:"); 
+        PrintArrayReverse(myArray, myArray.Length - 1); 
+    } 
+ 
+    static void PrintArrayReverse(int[] arr, int index) 
+    { 
+        if (index >= 0) 
+        { 
+            Console.Write(arr[index] + " "); 
+            PrintArrayReverse(arr, index - 1); 
+        } 
+    } 
+}
